b100ebcd9ac2463f78627b427f83d14faeb94c7a
rbsec <robin@rbsec.net>
2013-03-13 20:46:26 +0000

    Moved DNS lookup into method and added wildcard support

--- dnscan.py
+++ dnscan.py
@@ -3,22 +3,24 @@ import Queue
 import threading
 import dns.resolver
 import sys
-from time import sleep
 
 # Usage: dnscan.py <domain name> <wordlist>
 
 class queue_manager(threading.Thread):
+    global wildcard
     def __init__(self, queue):
         threading.Thread.__init__(self)
         self.queue = queue
 
     def get_name(self, domain):
+            global wildcard
             try:
                 sys.stdout.write(domain + "                              \r")
-                resolver = dns.resolver.Resolver()
-                resolver.timeout = 1
-                res = resolver.query(domain, 'A')
+                res = lookup(domain)
                 for rdata in res:
+                    if wildcard:
+                        if rdata.address == wildcard:
+                            return
                     print rdata.address + " - " + domain
                     add_target(domain)
             except:
@@ -44,8 +46,25 @@ def get_args():
     # Opens wordlist, read and strip carriage returns
     wordlist = open(sys.argv[2]).read().splitlines()
 
+def lookup(domain):
+    resolver = dns.resolver.Resolver()
+    resolver.timeout = 1
+    try:
+        res = resolver.query(domain, 'A')
+        return res
+    except:
+        return
+
+def get_wildcard(target):
+    res = lookup("nonexistantdomain" + "." + target)
+    if res:
+        print "[+] Wildcard IP found - " + res[0].address
+        return res[0].address
+
 def main():
+    global wildcard
     get_args()
+    wildcard = get_wildcard(target)
     for i in range(8):      # Number of threads
         t = queue_manager(queue)
         t.setDaemon(True)
